c11ed1dd938ba79f48abc03a14c6416933d0205b
Figo <Figoware@users.noreply.github.com>
2021-12-18 12:14:33 +0100

    Update PageRangeOptions.php

--- src/PopplerPhp/PopplerOptions/PageRangeOptions.php
+++ src/PopplerPhp/PopplerOptions/PageRangeOptions.php
@@ -7,13 +7,13 @@
  * Time:    6:06 PM
  **/
 
-namespace NcJoes\PopplerPhp\PopplerOptions;
+namespace Figoware\PopplerPhp\PopplerOptions;
 
-use NcJoes\PopplerPhp\Constants as C;
+use Figoware\PopplerPhp\Constants as C;
 
 /**
  * Trait PageRangeOptions
- * @package NcJoes\PopplerPhp\PopplerOptions
+ * @package Figoware\PopplerPhp\PopplerOptions
  */
 trait PageRangeOptions
 {
