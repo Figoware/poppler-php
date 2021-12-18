c52408b6ac3c964d206cb8e582f5e9d60d6c775d
Figo <Figoware@users.noreply.github.com>
2021-12-18 12:13:17 +0100

    Update CredentialOptions.php

--- src/PopplerPhp/PopplerOptions/CredentialOptions.php
+++ src/PopplerPhp/PopplerOptions/CredentialOptions.php
@@ -7,13 +7,13 @@
  * Time:    5:29 PM
  **/
 
-namespace NcJoes\PopplerPhp\PopplerOptions;
+namespace Figoware\PopplerPhp\PopplerOptions;
 
-use NcJoes\PopplerPhp\Constants as C;
+use Figoware\PopplerPhp\Constants as C;
 
 /**
  * Trait CredentialOptions
- * @package NcJoes\PopplerPhp\PopplerOptions
+ * @package Figoware\PopplerPhp\PopplerOptions
  */
 trait CredentialOptions
 {
