6e3727af193c4034003d81a7e98b04b7ddc5c49c
Figo <Figoware@users.noreply.github.com>
2021-12-18 12:13:29 +0100

    Update DateFlags.php

--- src/PopplerPhp/PopplerOptions/DateFlags.php
+++ src/PopplerPhp/PopplerOptions/DateFlags.php
@@ -7,13 +7,13 @@
  * Time:    6:37 PM
  **/
 
-namespace NcJoes\PopplerPhp\PopplerOptions;
+namespace Figoware\PopplerPhp\PopplerOptions;
 
-use NcJoes\PopplerPhp\Constants as C;
+use Figoware\PopplerPhp\Constants as C;
 
 /**
  * Trait DateFlags
- * @package NcJoes\PopplerPhp\PopplerOptions
+ * @package Figoware\PopplerPhp\PopplerOptions
  */
 trait DateFlags
 {
