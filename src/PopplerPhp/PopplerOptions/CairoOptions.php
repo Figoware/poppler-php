185f44c3ce3e86642ea695c3dc863dc6fa58894a
Figo <Figoware@users.noreply.github.com>
2021-12-18 12:12:47 +0100

    Update CairoOptions.php

--- src/PopplerPhp/PopplerOptions/CairoOptions.php
+++ src/PopplerPhp/PopplerOptions/CairoOptions.php
@@ -7,13 +7,13 @@
  * Time:    8:38 AM
  **/
 
-namespace NcJoes\PopplerPhp\PopplerOptions;
+namespace Figoware\PopplerPhp\PopplerOptions;
 
-use NcJoes\PopplerPhp\Constants as C;
+use Figoware\PopplerPhp\Constants as C;
 
 /**
  * Trait CairoOptions
- * @package NcJoes\PopplerPhp\PopplerOptions
+ * @package Figoware\PopplerPhp\PopplerOptions
  */
 trait CairoOptions
 {
