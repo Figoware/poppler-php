edcfea3567e1baac255abbe8793eef377b9677a1
Figo <Figoware@users.noreply.github.com>
2021-12-18 12:13:00 +0100

    Update ConsoleFlags.php

--- src/PopplerPhp/PopplerOptions/ConsoleFlags.php
+++ src/PopplerPhp/PopplerOptions/ConsoleFlags.php
@@ -7,13 +7,13 @@
  * Time:    1:08 AM
  **/
 
-namespace NcJoes\PopplerPhp\PopplerOptions;
+namespace Figoware\PopplerPhp\PopplerOptions;
 
-use NcJoes\PopplerPhp\Constants as C;
+use Figoware\PopplerPhp\Constants as C;
 
 /**
  * Trait ConsoleFlags
- * @package NcJoes\PopplerPhp\PopplerOptions
+ * @package Figoware\PopplerPhp\PopplerOptions
  */
 trait ConsoleFlags
 {
